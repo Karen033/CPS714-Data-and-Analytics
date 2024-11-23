8b54036b86c28b79af803becfee6fd45e48b5b76
wendyxie19 <128992104+wendyxie19@users.noreply.github.com>
2024-11-23 13:30:37 -0500

    Update server.js (prev vers)

--- CPS714/server.js
+++ CPS714/server.js
@@ -140,7 +140,7 @@ app.post('/api/generate-reports', async (req, res) => {
         }
       ]));
     
-      // Map over the usersData to include points_redeemed and redeemed_date
+      // Map over the usersData to include rewards_status and redeemed_date
       reportData = usersData.map((user) => {
         const userRewards = rewardsMap.get(user.user_id) || { points_redeemed: 0, redeemed_date: null };
         return {
@@ -149,29 +149,7 @@ app.post('/api/generate-reports', async (req, res) => {
           redeemed_date: userRewards.redeemed_date,  // Add redeemed_date to the report data
         };
       });
-    } 
-    else if (reportType === 'User Engagement - Earnings') {
-      const earningsMap = new Map(earningsData.map((earning) => [
-        earning.user_id, 
-        { 
-          points_earned: earning.points_earned || 0,
-          earned_date: earning.earned_date || null 
-        }
-      ]));
-
-      
-    
-      // Map over the usersData to include earned_date and points_earned
-      reportData = usersData.map((user) => {
-        const userEarnings = earningsMap.get(user.user_id) || {  points_earned: 0, earned_date: null };
-        return {
-          user_id: user.user_id,
-          points_earned: userEarnings.points_earned,
-          earned_date: userEarnings.earned_date,  
-        };
-      });
-    } 
-    else if (reportType === 'Ticket Metrics') {
+    } else if (reportType === 'Ticket Metrics') {
       const { data: ticketsData, error: ticketsError } = await supabase
         .from('05_support_ticket')
         .select('ticket_id, user_id, created_at, updated_at, status')
